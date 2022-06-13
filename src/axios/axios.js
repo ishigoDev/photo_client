6462f056860ac38275bf7254db5cd895d942e079
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-13 22:42:37 +0530

    changing-server-url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({   
-    baseURL:"http://localhost:2000/"
+    baseURL:"https://neutrography.herokuapp.com/"
 });
 
 export default instance;
